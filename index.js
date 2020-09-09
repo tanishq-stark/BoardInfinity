d06b78e22a514fb1c2ae77ed18a3fcd28104746b
Tanishq Mondal <52147425+tanishq-stark@users.noreply.github.com>
2020-09-10 02:00:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,39 @@ io.on('connection', function(socket) {
     io.emit('message', msg);
   });
 });
+
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Chat server running');
+}); 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NameForm from './src/NameForm'
+
+
+ReactDOM.render(
+    <div>
+    <NameForm />
+
+
+    </div>    
+    ,
+    
+  document.getElementById('root')
+);
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
